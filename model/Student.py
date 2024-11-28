b5d155f80426e56034b87de081d4dfd7f158bd0c
Mirim <marinrim23@gmail.com>
2024-11-28 13:23:15 +0900

    feat: Make new class Student

--- model/Student.py
+++ model/Student.py
@@ -6,3 +6,10 @@ class AbstractStudent(metaclass=ABC):
         self.password = password
         self.name = name
         self.department = department
+        self.lectures_registered_for = []
+
+class Student(AbstractStudent):
+    def register_lecture(self, Lecture):
+        self.lectures_registered_for.append(Lecture)
+
+
