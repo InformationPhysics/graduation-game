2ff8a62c342c2174411fcc93039c4b2a9b12b342
Mirim <marinrim23@gmail.com>
2024-12-09 12:56:14 +0900

    docs: Edit README.md

--- controller/auth_controller.py
+++
@@ -1,8 +0,0 @@
-from service.authentication import *
-from view.input_view import InputView
-
-class AuthController:
-    auth_service = AuthenticationService()
-
-    def sign_up(self):
-        student = InputView.print_sign_up()
